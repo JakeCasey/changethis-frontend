b84797e0ae6689924c2ef77ec199d77fda137c63
Jake Casey <acasestudy0@gmail.com>
2019-09-30 16:57:52 -0500

    error handling?

--- pages/api/proxy.js
+++ pages/api/proxy.js
@@ -1,11 +1,15 @@
 import { endpoint } from '../../config';
 
 export default async (req, res) => {
-  var url = req.url;
-  var fetchUrl = endpoint + url;
+  try {
+    var url = req.url;
+    var fetchUrl = endpoint + url;
 
-  var fetchRes = await fetch(fetchUrl);
-  var response = await fetchRes.text();
-  res.setHeader('Content-Type', 'text/html');
-  res.send(response);
+    var fetchRes = await fetch(fetchUrl);
+    var response = await fetchRes.text();
+    res.setHeader('Content-Type', 'text/html');
+    res.send(response);
+  } catch (error) {
+    if (error) throw new Error(error);
+  }
 };
