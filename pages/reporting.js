ce25949f57c8d1afcaa33e0be588198d2ff16bed
Jake Casey <acasestudy0@gmail.com>
2019-11-06 20:12:35 -0600

    removed reporting component

--- pages/reporting.js
+++ pages/reporting.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import PleaseSignIn from '../components/PleaseSignIn';
-import Reporting from '../components/special/Reporting';
+// import Reporting from '../components/special/Reporting';
 
 const ProtectedPage = props => (
   <div>
